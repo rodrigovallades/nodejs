99f2becdd55034dbaf7a48337c3ce2b23c7943fe
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-10-05 12:46:22 -0300

    ✨ Parse request paths

--- index.js
+++ index.js
@@ -6,9 +6,24 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // The server should respond to all requests with a string 
-var server = http.createServer((req,res) => res.end('Hello world\n'));
+var server = http.createServer((req,res) => {
+
+    var parsedUrl = url.parse(req.url, true);
+    var path = parsedUrl.pathname;
+    var trimmedPath = path.replace(/ˆ\/+|\/+$/g,'');
+
+    // Send response
+    res.end('Hello world\n')
+
+    // Log request path
+    console.log(`Request received on path: ${trimmedPath}`); 
+});
+
+
+
 
 // Start the server and have it listen on port 3000
 server.listen(3000, () => console.log('The server is listening on port 3000 now') )
