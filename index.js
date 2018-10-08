143a957a85812a0569c5f670954e1023961e20e3
Rodrigo Vallades <rodrigo.vallades@gmail.com>
2018-10-08 15:54:43 -0300

    ✨ Parse payloads

--- index.js
+++ index.js
@@ -7,6 +7,7 @@
 // Dependencies
 var http = require('http');
 var url = require('url');
+var StringDecoder = require('string_decoder').StringDecoder;
 
 // The server should respond to all requests with a string 
 var server = http.createServer((req,res) => {
@@ -17,17 +18,23 @@ var server = http.createServer((req,res) => {
     var method = req.method.toUpperCase();
     var queryStringObject = parsedUrl.query;
     var headers = req.headers;
+    var decoder = new StringDecoder('utf-8');
+    var buffer = '';
 
-    // Send response
-    res.end('Hello world\n')
+    // Get the payload, if any
+    req.on('data', data => buffer += decoder.write(data))
+    req.on('end', () => {
+        buffer += decoder.end();
 
-    // Log request path
-    console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters:`, queryStringObject); 
-    console.log(`Request headers:`, headers);
+        // Send response
+        res.end('Hello world\n')
+
+        // Log request path
+        console.log(`Request received on path: ${trimmedPath} with method: ${method} and with these query string parameters:`, queryStringObject); 
+        console.log(`Request headers:`, headers);
+        console.log(`Request payload:`, buffer); 
+    });
 });
-
-
-
 
 // Start the server and have it listen on port 3000
 server.listen(3000, () => console.log('The server is listening on port 3000 now') )
